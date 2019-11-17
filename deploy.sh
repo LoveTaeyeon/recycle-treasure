dee1cbe6d66081fb6e32f5361df5580c133d4182
HanShuo <shuo@narvii.com>
2019-11-17 11:27:15 +0000

    [Misc] Add deploy shell

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 cd /project/source-code/recycle-treasure
 
-git fetch --all -q && git reset --hard origin/master -q && git checkout -f -q && git checkout master -q && git pull --no-edit -q
+git fetch --all -q && git reset --hard origin/master -q && git checkout -f -q && git checkout master -q && git pull
 echo 'git pull master finished !'
 
 mvn clean install -Pprod
